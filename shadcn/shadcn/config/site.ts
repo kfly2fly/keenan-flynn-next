81e4863713f80fc477a14d51775908237bee4449
Keenan Flynn <k.p.flynn70@gmail.com>
2023-12-28 10:59:31 -0600

    feat(template): Initial boilerplate with header and lightmode/darkmode

---
+++ shadcn/shadcn/config/site.ts
@@ -0,0 +1,29 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "KeenanFlynn.com",
+  description:
+    "A personal space where I share things I'm interested in.",
+  mainNav: [
+    {
+      title: "Resume",
+      href: "/resume",
+    },
+    {
+      title: "Projects",
+      href: "/projects",
+    },
+    {
+      title: "Books",
+      href: "/books",
+    },
+    {
+      title: "Travel",
+      href: "/travel",
+    },
+  ],
+  links: {
+    linkedin: "https://linkedin.com/in/keenanpflynn",
+    github: "https://github.com/kfly2fly",
+  },
+}
