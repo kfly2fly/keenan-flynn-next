303a62edb66cff3b9f1ec9b0aee6283721347d8c
Keenan Flynn <k.p.flynn70@gmail.com>
2024-02-19 20:25:54 -0600

    feat(projects): add project data + project card

---
+++ app/projects/project-data.tsx
@@ -0,0 +1,21 @@
+export const PROJECT_DATA = [
+    {
+        title: "My first Flask website",
+        url: "flask-blog",
+        githubUrl: "https://github.com/kfly2fly/Flask-Web-Repo",
+        hostedUrl: "https://flask-site-self.vercel.app/",
+        date_finished: "November 2022",
+        image: {
+            screenshot: "/projects/flask-blog.png",
+            imageAlt: "A screenshot of my Flask Blog"
+        },
+        description: "My first Flask Site",
+        summary: "My first Flask Site",
+        markdown: undefined,
+        skills: [
+            "Python",
+            "Flask",
+            "HTML",
+        ]
+    }
+]
---
+++ components/projects/project-card.tsx
@@ -0,0 +1,77 @@
+import Image from "next/image"
+import Link from "next/link"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+import { Icons } from "../header/icons"
+import { BookIcon } from "../icons/book"
+import { Badge, badgeVariants } from "../ui/badge"
+
+interface ProjectCardProps {
+  title: string
+  url: string
+  githubUrl: string
+  hostedUrl: string
+  date_finished: string
+  image: {
+    screenshot: string
+    imageAlt: string
+  }
+  description: string
+  summary: string
+  markdown: string | undefined
+  skills: string[]
+}
+
+export default function ProjectCard(project: ProjectCardProps) {
+  return (
+    <Link href={`/projects/${project.url}`} scroll>
+      <Card className="bg-muted max-w-96 h-72 hover:bg-accent hover:border-primary">
+        <CardHeader className="pb-3 w-full flex flex-row justify-between items-start">
+          <div>
+            <CardTitle className="text-accent-foreground">
+              {project.title}
+            </CardTitle>
+            <CardDescription className="my-1 italic">
+              {project.date_finished}
+            </CardDescription>
+            <div className="flex flex-row gap-1">
+              {project.skills.map((skill) => {
+                return <Badge key={skill} className="bg-accent hover:bg-muted" variant='secondary'>{skill}</Badge>
+              })}
+            </div>
+          </div>
+          {/* <div className="flex flex-row">
+            {book.favorite ? (
+              <Icons.star className="fill-yellow-300 stroke-yellow-400" />
+            ) : null}
+            {book.bookclub ? (
+              <BookIcon className="ml-2 size-6 fill-accent-foreground" />
+            ) : null}
+          </div> */}
+        </CardHeader>
+        <CardContent className="h-3/4">
+          <div className="flex flex-col gap-4 h-full">
+            <div className="relative w-full">
+              <Image
+                src={project.image.screenshot}
+                alt=""
+                // fill
+                width={384}
+                height={190.5}
+                className="object-contain"
+              />
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+    </Link>
+  )
+}
