6cd2f02e68815ab00323f943869840727d301e0b
Keenan Flynn <k.p.flynn70@gmail.com>
2024-09-29 09:46:10 -0500

    feat(projects): add 2 more projects

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -1,100 +1,115 @@
 export const PROJECT_DATA = [
-    {
-        title: "HTTP Server with C++",
-        url: "cpp-http",
-        description: "An HTTP Server built from scratch with C++",
-        githubUrl: "https://github.com/kfly2fly/web-server-cpp",
-        markdownUrl: "https://raw.githubusercontent.com/kfly2fly/web-server-cpp/main/README.md",
-        hostedUrl: null,
-        date_finished: new Date(2024, 5),
-        image: {
-            screenshot: "/projects/http-c++.png",
-            imageAlt: "HTTP with C++ logo"
-        },
-        
-        skills: [
-            "C++",
-            "HTTP",
-            "Sockets",
-            ]
-    },
-    {
-        title: "KeenanFlynn.com",
-        url: "next-site",
-        description: "My personal website built with NextJS and TailwindCSS",
-        githubUrl: "https://github.com/kfly2fly/keenan-flynn-next",
-        markdownUrl: "https://raw.githubusercontent.com/kfly2fly/keenan-flynn-next/master/README.md",
-        hostedUrl: null,
-        date_finished: new Date(2024, 1),
-        image: {
-            screenshot: "/projects/keenan-flynn-next.png",
-            imageAlt: "A screenshot of this website"
-        },
-        
-        skills: [
-            "TypeScript",
-            "NextJS",
-            "Tailwind CSS",
-            "OAuth",
-        ]
-    },
-    {
-        title: "Tunelink",
-        url: "tunelink",
-        description: "A class project that finds music events in your area based on Spotify preference",
-        githubUrl: "https://github.com/kfly2fly/tunelink",
-        markdownUrl: "https://raw.githubusercontent.com/kfly2fly/tunelink/master/README.md",
-        hostedUrl: "https://tunelink-snowy.vercel.app/",
-        date_finished: new Date(2022, 4),
-        image: {
-            screenshot: "/projects/tunelink.png",
-            imageAlt: "A screenshot of Tunelink"
-        },
-        
-        skills: [
-            "Javascript",
-            "Angular",
-            "Rest APIs",
-            "Spotify",
-        ]
-    },
-    {
-        title: "My first Flask website",
-        url: "flask-blog",
-        description: "An HTML site with a Python-Flask backend. My first experience with web design",
-        githubUrl: "https://github.com/kfly2fly/Flask-Web-Repo",
-        markdownUrl: "https://raw.githubusercontent.com/kfly2fly/Flask-Web-Repo/master/README.md",
-        hostedUrl: "https://flask-site-self.vercel.app/",
-        date_finished: new Date(2020, 11),
-        image: {
-            screenshot: "/projects/flask-blog.png",
-            imageAlt: "A screenshot of my Flask Blog"
-        },
-        skills: [
-            "Python",
-            "Flask",
-            "HTML",
-            "Bootstrap"
-        ]
-    },
-    {
-        title: "DL Quote Generator",
-        url: "quote-gen",
-        description: "An LSTM Deep Learning model that produces quotes by a fictional person. Includes HTML frontend with a Python-Flask backend.",
-        githubUrl: "https://github.com/kfly2fly/PyDLQuotesProj",
-        markdownUrl: "https://raw.githubusercontent.com/kfly2fly/PyDLQuotesProj/main/README.md",
-        hostedUrl: null,
-        date_finished: new Date(2022, 3),
-        image: {
-            screenshot: "/projects/public/input.png",
-            imageAlt: "A screenshot of the DL quote gen app"
-        },
-        skills: [
-            "Python",
-            "Generative",
-            "LSTM",
-            "Flask",
-        ]
+  {
+    title: "HTTP Server with C++",
+    url: "cpp-http",
+    description: "An HTTP Server built from scratch with C++",
+    githubUrl: "https://github.com/kfly2fly/web-server-cpp",
+    markdownUrl:
+      "https://raw.githubusercontent.com/kfly2fly/web-server-cpp/main/README.md",
+    hostedUrl: null,
+    date_finished: new Date(2024, 5),
+    image: {
+      screenshot: "/projects/http-c++.png",
+      imageAlt: "HTTP with C++ logo",
     },
 
+    skills: ["C++", "HTTP", "Sockets"],
+  },
+  {
+    title: "Arduino Playground",
+    url: "arduino",
+    description: "A collection of Arduino sketches",
+    githubUrl: "https://github.com/kfly2fly/ArduinoSketches",
+    markdownUrl:
+      "https://raw.githubusercontent.com/kfly2fly/ArduinoSketches/master/README.md",
+    hostedUrl: null,
+    date_finished: new Date(2024, 3),
+    image: {
+      screenshot: "/projects/arduino.jpg",
+      imageAlt: "Arduino img",
+    },
+
+    skills: ["Arduino", "C++", "Circuits"],
+  },
+  {
+    title: "KeenanFlynn.com",
+    url: "next-site",
+    description: "My personal website built with NextJS and TailwindCSS",
+    githubUrl: "https://github.com/kfly2fly/keenan-flynn-next",
+    markdownUrl:
+      "https://raw.githubusercontent.com/kfly2fly/keenan-flynn-next/master/README.md",
+    hostedUrl: null,
+    date_finished: new Date(2024, 1),
+    image: {
+      screenshot: "/projects/keenan-flynn-next.png",
+      imageAlt: "A screenshot of this website",
+    },
+
+    skills: ["TypeScript", "NextJS", "Tailwind CSS", "OAuth"],
+  },
+  {
+    title: "Tunelink",
+    url: "tunelink",
+    description:
+      "A class project that finds music events in your area based on Spotify preference",
+    githubUrl: "https://github.com/kfly2fly/tunelink",
+    markdownUrl:
+      "https://raw.githubusercontent.com/kfly2fly/tunelink/master/README.md",
+    hostedUrl: "https://tunelink-snowy.vercel.app/",
+    date_finished: new Date(2022, 4),
+    image: {
+      screenshot: "/projects/tunelink.png",
+      imageAlt: "A screenshot of Tunelink",
+    },
+
+    skills: ["Javascript", "Angular", "Rest APIs", "Spotify"],
+  },
+  {
+    title: "My first Flask website",
+    url: "flask-blog",
+    description:
+      "An HTML site with a Python-Flask backend. My first experience with web design",
+    githubUrl: "https://github.com/kfly2fly/Flask-Web-Repo",
+    markdownUrl:
+      "https://raw.githubusercontent.com/kfly2fly/Flask-Web-Repo/master/README.md",
+    hostedUrl: "https://flask-site-self.vercel.app/",
+    date_finished: new Date(2020, 11),
+    image: {
+      screenshot: "/projects/flask-blog.png",
+      imageAlt: "A screenshot of my Flask Blog",
+    },
+    skills: ["Python", "Flask", "HTML", "Bootstrap"],
+  },
+  {
+    title: "DL Quote Generator",
+    url: "quote-gen",
+    description:
+      "An LSTM Deep Learning model that produces quotes by a fictional person. Includes HTML frontend with a Python-Flask backend.",
+    githubUrl: "https://github.com/kfly2fly/PyDLQuotesProj",
+    markdownUrl:
+      "https://raw.githubusercontent.com/kfly2fly/PyDLQuotesProj/main/README.md",
+    hostedUrl: null,
+    date_finished: new Date(2022, 3),
+    image: {
+      screenshot: "/projects/public/input.png",
+      imageAlt: "A screenshot of the DL quote gen app",
+    },
+    skills: ["Python", "Generative", "LSTM", "Flask"],
+  },
+  {
+    title: "Oil Well Data Analysis",
+    url: "well-data-analysis",
+    description: "A data analysis focused around oil well production.",
+    githubUrl: "https://github.com/kfly2fly/Well-Data-Analysis",
+    markdownUrl:
+      "https://raw.githubusercontent.com/kfly2fly/Well-Data-Analysis/main/README.md",
+    hostedUrl: null,
+    date_finished: new Date(2021, 4),
+    image: {
+      screenshot: "/projects/well-data-analysis.png",
+      imageAlt: "A chart from the project",
+    },
+
+    skills: ["Python", "ML/DL", "Scikit Learn"],
+  },
 ]
